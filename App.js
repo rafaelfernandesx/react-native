1ec4d61760ede01e867472631255970d97565d57
Rafael Fernandes <rafamex.mex@gmail.com>
2020-08-08 22:27:34 -0300

    flexbox basico

--- App.js
+++ App.js
@@ -2,22 +2,24 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
-  background-color: #02f;
-  font-size: 30px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   flex: 1;
 `;
 
-const Texto = styled.Text`
-  color: ${props=>props.cor};
-  font-size: 30px;
-  background-color: #222;
+const Squad = styled.View`
+  height: 50px;
+  width: 50px;
+  background-color: ${(props) => props.cor};
 `;
 
 export default () => {
   return (
     <Page>
-      <Texto cor="red">Texo exemplo</Texto>
-      <Texto cor="gray">Hello World</Texto>
+      <Squad cor="red"></Squad>
+      <Squad cor="green"></Squad>
+      <Squad cor="blue"></Squad>
     </Page>
   );
 };
